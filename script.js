bb15240d3ee1401a29ed22584903efa26543b661
Thiago Cordeiro <thiagocordeiro2512@gmail.com>
2025-08-07 09:13:40 -0300

    Adicionando logica de excluir e alterar quantidade

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let listaProduto = JSON.parse(localStorage.getItem("Produtos")) || [];
 
+let qtd = 1;
 function adicionarCarrinho(nome, valor) {
-    window.alert("adicionado ao carrinho")
-    const produto = { nome, valor };
-    listaProduto.push(produto)
-    localStorage.setItem("Produtos", JSON.stringify(listaProduto));
+    window.alert("adicionado ao carrinho");
+
+    let lista = JSON.parse(localStorage.getItem("Produtos")) || [];
+    const index = lista.findIndex(p => p.nome === nome);
+
+    if (index !== -1) {
+        lista[index].qtd += 1;
+    } else {
+        lista.push({ nome, valor, qtd: 1 });
+    }
+
+    localStorage.setItem("Produtos", JSON.stringify(lista));
 }
 
 function listarCarrinho() {
@@ -15,24 +24,59 @@ function listarCarrinho() {
 
     let total = 0;
 
+    secaoProdutos.innerHTML = "";
+    
     for (let i = 0; i < lista.length; i++) {
-        secaoProdutos.innerHTML += `
-        
-            <hr class="linha">
-            <div class="produtoDescricao">
-                <div class="nomeProdutoCarrinho"><p>${lista[i].nome}</p></div>
-                <div class="precoProdutoCarrinho"><p>${lista[i].valor}</p></div>
-                <div class="quantidadeProdutoCarrinho"><p>1</p></div>
+    secaoProdutos.innerHTML += `
+        <hr class="linha">
+        <div class="produtoDescricao">
+            <div class="nomeProdutoCarrinho"><p>${lista[i].nome}</p></div>
+            <div class="precoProdutoCarrinho"><p>${lista[i].valor}</p></div>
+            <div class="quantidadeProdutoCarrinho">
+                <button onclick="alterarQtd(${i}, -1)">➖</button>
+                <span>${lista[i].qtd}</span>
+                <button onclick="alterarQtd(${i}, 1)">➕</button>
             </div>
-            <hr class="linha">
-        `;
-        total += lista[i].valor
-    }
+            <div class="removerProdutoCarrinho">
+                <button onclick="removerItem(${i})">🗑️</button>
+            </div>
+        </div>
+        <hr class="linha">
+    `;
+    total += lista[i].valor * lista[i].qtd;;
+}
     totalInfos.innerHTML = `
             <h1>Total R$:</h1>
             <h2>${total}</h2>
         `;
 }
+
+function removerItem(index) {
+    let lista = JSON.parse(localStorage.getItem("Produtos")) || [];
+
+    if (index >= 0 && index < lista.length) {
+        lista.splice(index, 1);
+        localStorage.setItem("Produtos", JSON.stringify(lista));
+        listarCarrinho();
+    }
+}
+
+function alterarQtd(index, aux) {
+    let lista = JSON.parse(localStorage.getItem("Produtos")) || [];
+
+    if (index >= 0 && index < lista.length) {
+        lista[index].qtd += aux;
+
+        
+        if (lista[index].qtd <= 0) {
+            lista.splice(index, 1);
+        }
+
+        localStorage.setItem("Produtos", JSON.stringify(lista));
+        listarCarrinho();
+    }
+}
+
 
 function finalizar() {
     window.alert("Compra finalizada")
